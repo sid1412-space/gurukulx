92b73eb8b123522b0065820b6e315f5f468b4535
SIDDHARTH KEDIA <bkedia36@gmail.com>
2025-08-12 10:59:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/session/layout.tsx
+++ src/app/session/layout.tsx
@@ -1,27 +1,11 @@
 'use client';
 
-import { cn } from '@/lib/utils';
-import { Toaster } from '@/components/ui/toaster';
+import React from 'react';
 
 export default function SessionLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="!scroll-smooth h-full">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        ></link>
-      </head>
-      <body className={cn('font-body antialiased bg-background h-full w-full overflow-hidden')}>
-        {children}
-        <Toaster />
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
