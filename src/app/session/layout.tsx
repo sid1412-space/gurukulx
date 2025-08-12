e0b84a0d67a34825224776fef92f2d73e3d5b32a
SIDDHARTH KEDIA <bkedia36@gmail.com>
2025-08-12 11:16:38 +0000

    remove this top in session

--- src/app/session/layout.tsx
+++ src/app/session/layout.tsx
@@ -1,11 +1,29 @@
 'use client';
 
 import React from 'react';
+import { Toaster } from '@/components/ui/toaster';
+import { cn } from '@/lib/utils';
+import '../globals.css';
 
 export default function SessionLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
+       <body className={cn('font-body antialiased bg-background')}>
+          {children}
+          <Toaster />
+      </body>
+    </html>
+  );
 }
