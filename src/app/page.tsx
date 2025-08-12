2bf175d50cfad459600a557f14f390b01df5b4a5
SIDDHARTH KEDIA <bkedia36@gmail.com>
2025-08-12 11:20:19 +0000

    404 This page could not be found.

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/landing/Hero';
+import Features from '@/components/landing/Features';
+import Cta from '@/components/landing/Cta';
+
+export default function LandingPage() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Cta />
+    </>
+  );
+}
